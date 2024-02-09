008959bf2c9cf91b09f4851980386948f98ce3b5
Joesephats <79176095+Joesephats@users.noreply.github.com>
2024-02-09 16:12:26 -0500

    Finished everything but menu and end game screens

--- Assets/Scripts/Minesweeper.cs
+++ Assets/Scripts/Minesweeper.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,8 +10,15 @@ public class Minesweeper : MonoBehaviour
     bool[,] bombGrid = new bool[5, 5];
     GameObject[,] buttonGrid = new GameObject[5, 5];
     [SerializeField] GameObject tile;
+    [SerializeField] GameObject adjacentBombUI;
+    [SerializeField] GameObject bombUI;
     [SerializeField] Transform canvasTransfrom;
 
+    int tilesChecked = 0;
+
+    bool gameOver = false;
+
+    bool gameWon = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +29,11 @@ public class Minesweeper : MonoBehaviour
                 GameObject currentTile = Instantiate(tile, canvasTransfrom, false);
 
                 buttonGrid[i, j] = currentTile;
-                currentTile.transform.position = new Vector2((i * 100f) + 350, (j * 100f) + 75);
+                currentTile.transform.position = new Vector2((j * 100f) + 350, (i * 100f) + 75);
                 Debug.Log($"Tile: ({i}, {j}) at Pos: {currentTile.transform.position.x}, {currentTile.transform.position.y}");
                 currentTile.GetComponent<Tile>().row = i;
                 currentTile.GetComponent<Tile>().col = j;
+                currentTile.GetComponent<Tile>().tilePosition = currentTile.transform.position;
 
                 //zzz
                 currentTile.GetComponent<Button>().onClick.AddListener(() => TileClicked(currentTile.GetComponent<Tile>().row, currentTile.GetComponent<Tile>().col));
@@ -42,7 +51,29 @@ public class Minesweeper : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameOver)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (buttonGrid[i, j] != null)
+                    {
+                        buttonGrid[i, j].GetComponent<Button>().interactable = false;
+                    }
+                }
+            }
+        }
+
+        if (tilesChecked == 20)
+        {
+            gameWon = true;
+        }
+
+        if (gameWon)
+        {
+
+        }
     }
 
     void InitializeBoolArray(bool[,] array)
@@ -82,50 +113,313 @@ public class Minesweeper : MonoBehaviour
 
         if (CheckBombUnderTile(row, column))
         {
+            gameOver = true;
             Debug.Log("GameOver");
+
+            GameObject ui = Instantiate(bombUI, canvasTransfrom, false);
+            ui.transform.position = buttonGrid[row, column].GetComponent<Tile>().tilePosition;
+        }
+        else
+        {
+            Debug.Log(CheckForAdjacentBombs(row, column));
+
+            GameObject ui = Instantiate(adjacentBombUI, canvasTransfrom, false);
+            ui.transform.position = buttonGrid[row, column].GetComponent<Tile>().tilePosition;
+            ui.GetComponentInChildren<TMP_Text>().text = $"{CheckForAdjacentBombs(row, column)}";
+
         }
 
-        Debug.Log(CheckForAdjacentBombs(row, column));
+        buttonGrid[row, column].SetActive(false);
     }
 
-        //will return out of range exception. 
+    //will return out of range exception. 
     int CheckForAdjacentBombs(int row, int column)
     {
         int adjacentBombCounter = 0;
+        int above = row + 1;
+        int below = row - 1;
+        int right = column + 1;
+        int left = column - 1;
 
-        if (bombGrid[row+1, column])
+   
+        if (above == 5)
         {
-            adjacentBombCounter++;
-        }
-        if (bombGrid[row-1, column]) 
-        {
-            adjacentBombCounter++;        
-        }
-        if (bombGrid[row, column + 1])
-        {
-            adjacentBombCounter++;
-        }
-        if (bombGrid[row, column - 1])
-        {
-            adjacentBombCounter++;
-        }
-        if (bombGrid[row+1, column + 1])
-        {
-            adjacentBombCounter++;
-        }
-        if (bombGrid[row+1, column - 1])
-        {
-            adjacentBombCounter++;
-        }
-        if (bombGrid[row-1, column + 1])
-        {
-            adjacentBombCounter++;
-        }
-        if (bombGrid[row-1, column - 1])
-        {
-            adjacentBombCounter++;
+            //top right
+            if (right == 5)
+            {
+                if (bombGrid[row, left])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[below, column])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[below, left])
+                {
+                    adjacentBombCounter++;
+                }
+            }
+            //top left
+            else if (left == -1)
+            {
+                if (bombGrid[row, right])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[below, column])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[below, right])
+                {
+                    adjacentBombCounter++;
+                }
+            }
+            //top middles
+            else
+            {
+                if (bombGrid[row, left])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[row, right])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[below, left])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[below, column])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[below, right])
+                {
+                    adjacentBombCounter++;
+                }
+            }
+
         }
 
+        else if (below == -1)
+        {
+            //bottom right
+            if (right == 5)
+            {
+                if (bombGrid[row, left])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[above, left])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[above, column])
+                {
+                    adjacentBombCounter++;
+                }
+            }
+            //bottom left
+            else if (left == -1)
+            {
+                if (bombGrid[row, right])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[above, right])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[above, column])
+                {
+                    adjacentBombCounter++;
+                }
+            }
+            //bottom middles
+            else
+            {
+                if (bombGrid[row, left])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[row, right])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[above, left])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[above, column])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[above, right])
+                {
+                    adjacentBombCounter++;
+                }
+            }
+
+        }
+
+        else if (left == -1)
+        {
+            //top left
+            if (above == 5)
+            {
+                if (bombGrid[row, right])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[below, column])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[below, right])
+                {
+                    adjacentBombCounter++;
+                }
+            }
+            //bottom left
+            else if (below == -1)
+            {
+                if (bombGrid[row, right])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[above, right])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[above, column])
+                {
+                    adjacentBombCounter++;
+                }
+            }
+            //left middles
+            else
+            {
+                if (bombGrid[below, column])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[below, right])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[row, right])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[above, column])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[above, right])
+                {
+                    adjacentBombCounter++;
+                }
+            }
+        }
+
+        else if (right == 5)
+        {
+            //top right
+            if (above == 5)
+            {
+                if (bombGrid[row, left])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[below, column])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[below, left])
+                {
+                    adjacentBombCounter++;
+                }
+            }
+            //bottom right
+            else if (below == -1)
+            {
+                if (bombGrid[row, left])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[above, left])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[above, column])
+                {
+                    adjacentBombCounter++;
+                }
+            }
+            //right middles
+            else
+            {
+                if (bombGrid[below, column])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[below, left])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[row, left])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[above, column])
+                {
+                    adjacentBombCounter++;
+                }
+                if (bombGrid[above, left])
+                {
+                    adjacentBombCounter++;
+                }
+            }
+        }
+
+        else
+        {
+            if (bombGrid[above, column])
+            {
+                adjacentBombCounter++;
+            }
+            if (bombGrid[below, column])
+            {
+                adjacentBombCounter++;
+            }
+            if (bombGrid[row, right])
+            {
+                adjacentBombCounter++;
+            }
+            if (bombGrid[row, left])
+            {
+                adjacentBombCounter++;
+            }
+            if (bombGrid[above, right])
+            {
+                adjacentBombCounter++;
+            }
+            if (bombGrid[above, left])
+            {
+                adjacentBombCounter++;
+            }
+            if (bombGrid[below, right])
+            {
+                adjacentBombCounter++;
+            }
+            if (bombGrid[below, left])
+            {
+                adjacentBombCounter++;
+            }
+        } 
+        
         return adjacentBombCounter;
     }
 
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Tile : MonoBehaviour
 {
     public int row;
     public int col;
+
+    public Vector2 tilePosition;
 }
